c393d973cbbdfc1b842b067d7e01baa92a03752a
Monti2P <montillet.p@gmail.com>
2026-09-18 23:02:12 -0500

    Add service worker

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = "prize-points-v1";
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"])));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
